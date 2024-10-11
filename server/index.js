9f598a7a1553e3a366483f41e606209b7f715747
unknown <chavann717@gmail.com>
2024-10-11 23:51:39 +0530

    cookie config

--- server/index.js
+++ server/index.js
@@ -24,9 +24,11 @@ app.use(
     resave: false,
     saveUninitialized: false,
     secret:  process.env.SESSION_SECRET,
-    cookie:{
-      secure: true
-    }
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      secure: "auto", //Enable when deployment OR when not using localhost, this wont work without https
+      sameSite: "none", //Enable when deployment OR when not using localhost, We're not on the same site, we're using different site so the cookie need to effectively transfer from Backend to Frontend
+    },
   })
 );
 
